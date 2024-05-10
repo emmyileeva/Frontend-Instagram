c11e3740b874f400af08a9580244646a6620cd58
Emmy <emilyileeva@gmail.com>
2024-05-10 09:29:31 -0700

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-function App() {
-  return (
-    <>
-      <div>
-        <h1>Hi</h1>
-      </div>
-    </>
-  );
-}
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello InstaClone!</h1>;
+};
 
 export default App;
