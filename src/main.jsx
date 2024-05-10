7c6e148fe9f483cf1efcb5841d82666894c9a81e
Emmy <emilyileeva@gmail.com>
2024-05-10 07:38:08 -0700

    set up frontend file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.querySelector("#root")).render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>
+);
