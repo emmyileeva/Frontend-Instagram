a91d66499afb8b71a2ba6224bd507bc524c9a478
Emmy <emilyileeva@gmail.com>
2024-05-10 11:53:41 -0700

    implement router, auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import "./tailwind.css";
+import "./globals.css";
+import Home from "./pages/Home";
+import SignIn from "./auth/SignIn";
+import SignUp from "./auth/SignUp";
+import AuthLayout from "./auth/AuthLayout";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello InstaClone!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
