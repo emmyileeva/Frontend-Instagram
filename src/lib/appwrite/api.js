178b4366e70947821e6a0e0bad3108c807964216
Emmy <emilyileeva@gmail.com>
2024-05-13 08:29:12 -0700

    connect user to database

--- src/lib/appwrite/api.js
+++ src/lib/appwrite/api.js
@@ -1,16 +1,57 @@
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user) {
   try {
+    const avatarUrl = avatars.getInitials(user.name);
+
     const newAccount = await account.create(
       ID.unique(),
       user.email,
       user.password,
       user.name
     );
+
+    if (!newAccount) throw new Error("Failed to create user account");
+
+    const savedUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      imageUrl: avatarUrl,
+      username: user.username,
+    });
+
+    console.log("User account created:", newAccount);
+    console.log("User data saved to database:", savedUser);
+
     return newAccount;
   } catch (error) {
-    console.error(error);
+    console.error("Error creating user account:", error);
+    throw error;
+  }
+}
+
+export async function saveUserToDB(user) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user,
+      {
+        accountId: user.accountId,
+        name: user.name,
+        email: user.email,
+        imageUrl: user.imageUrl,
+        username: user.username,
+      }
+    );
+
+    console.log("User saved to database:", newUser);
+    return newUser;
+  } catch (error) {
+    console.error("Error saving user data to database:", error);
+    throw error;
   }
 }
