d4de28a7400e76c5d29e73b18adc86ad6b0aa5e4
Emmy <emilyileeva@gmail.com>
2024-05-17 10:35:20 -0700

    users.jsx

--- src/main/pages/Users.jsx
+++ src/main/pages/Users.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import UserCard from "@/components/shared/UserCard";
+import { useToast } from "@/components/ui/use-toast";
+import { useGetUsers } from "@/lib/react-query/queries";
 
-const Users = () => {
-  return <div>Users</div>;
+const AllUsers = () => {
+  const { toast } = useToast();
+
+  const { data: creators, isError: isErrorCreators } = useGetUsers();
+
+  if (isErrorCreators) {
+    toast({ title: "Something went wrong." });
+    return null;
+  }
+
+  return (
+    <div className="common-container px-4 md:px-0 md:ml-64 mt-4">
+      <h2 className="h3-bold md:h2-bold text-center w-full py-4">All Users</h2>
+      <div className="user-container grid grid-cols-2 md:grid-cols-4 gap-4">
+        {creators?.documents.map((creator) => (
+          <div key={creator?.$id} className="flex flex-col items-center">
+            <UserCard user={creator} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Users;
+export default AllUsers;
